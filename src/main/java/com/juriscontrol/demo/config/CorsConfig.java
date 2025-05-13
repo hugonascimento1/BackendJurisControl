7652ccacb7fdff7cb026c52b121fe3699c672b72
Matheus Vinícius <matheus678brdef@gmail.com>
2025-05-13 20:19:39 -0300

    ajuste na classe cors

--- src/main/java/com/juriscontrol/demo/config/CorsConfig.java
+++ src/main/java/com/juriscontrol/demo/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("https://juriscontrol.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
